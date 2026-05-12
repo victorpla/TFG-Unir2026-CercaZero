14552660ee1b37ab0b5d9aaf617f8702bb7efb4a
victorpla <victorplasoler@gmail.com>
2026-05-12 13:08:05 +0200

    Add login route and create item and update status of item

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -47,3 +47,46 @@ exports.getNearbyItems = async (req, res) => {
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
+
+
+exports.createItem = async (req, res) => {
+  try {
+    const { title, description, category, lng, lat } = req.body;
+
+    const newItem = new Item({
+      title,
+      description,
+      category,
+      donorId: req.user.id, // Obtenido del token JWT a través del middleware
+      exactLocation: {
+        type: 'Point',
+        coordinates: [parseFloat(lng), parseFloat(lat)]
+      }
+    });
+
+    await newItem.save();
+    res.status(201).json({ message: 'Objeto publicado con éxito', item: newItem });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al publicar el objeto' });
+  }
+};
+
+exports.updateItemStatus = async (req, res) => {
+  try {
+    const { status } = req.body; // 'AVAILABLE', 'RESERVED' o 'DELIVERED'
+    const item = await Item.findById(req.params.id);
+
+    if (!item) return res.status(404).json({ error: 'Objeto no encontrado' });
+    
+    // Solo el dueño puede cambiar el estado
+    if (item.donorId.toString() !== req.user.id) {
+      return res.status(401).json({ error: 'No autorizado' });
+    }
+
+    item.status = status;
+    await item.save();
+    res.json({ message: 'Estado actualizado', item });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar' });
+  }
+};
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
-// router.post('/login', authController.login); // Implementar cuando rellenes el controlador
+router.post('/login', authController.login);
+// Crear ítem (Protegido con token)
+router.post('/', auth, itemController.createItem);
+// Actualizar estado (Protegido con token)
+router.put('/:id/status', auth, itemController.updateItemStatus);
 
 module.exports = router;
